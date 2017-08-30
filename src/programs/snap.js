7de4b419cfb13a5d6333192fb33adf01ef29f16d
Alec Danaher <alec.danaher@gmail.com>
2017-08-29 20:13:50 -0400

    Moved eligibility functions into new folder

---
+++ src/programs/snap.js
@@ -0,0 +1,15 @@
+import { percentPovertyLevel, 
+    percentStateMedianIncome } from '.programs/helperFunctions';
+    
+function getSnapEligibility(client) {
+    let percentPov = percentPovertyLevel(parseInt(client.annualIncome), client.householdSize);
+    if (client.annualIncome == 0 || percentPov < 70) {
+        return {result: 'good', details: 'All good!', benefitValue: 1000};
+    } else if ( percentPov > 70 && percentPov < 80) {
+        return {result: 'information', details: `Your income puts you at ${percentPov.toFixed()}% of the federal poverty level, which is close to the 80% limit.`, benefitValue: 1000};
+    } else {
+        return {result: 'warning', details: `Your income puts you at ${percentPov.toFixed()}% of the federal poverty level, which is above the 80% limit.`, benefitValue: 0};
+    }
+}
+
+export {getSnapEligibility};
