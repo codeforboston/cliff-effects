5f8c68bcb8a89acf6d567171ff97eac907e1b243
knod <knodding@gmail.com>
2017-09-18 08:32:14 -0400

    Adds the expenses step left out of previous commit

--- src/forms/previousExpenses.js
+++ src/forms/previousExpenses.js
@@ -38,53 +38,53 @@ const Housing = function ( props ) {
 
   var client      = props.props.pageState,
       time        = props.time,
-      storeBool   = props.props.storeBoolean,
+      storeBool   = props.props.storeChecked,
       sharedProps = {
         client: client, type: props.type, time: time,
         storeComplex: props.props.storeComplex
       };
 
   // `hasHousing` is actually whether they're in the housing voucher program
-  var ownedAHome  = client.previousHomeowner,
-      wasHomeless = client.previousHomeless && !client.hasHousing && !ownedAHome;
+  var ownedAHome  = client[ time + 'Homeowner' ],
+      wasHomeless = client[ time + 'Homeless' ] && !client.hasHousing && !ownedAHome;
 
-  var utils = client.previousPaidUtilities, climate = client.previousGotClimateControl,
-      electricity = client.previousNonHeatElectricity, phone = client.previousTelephone;
+  var utils = client[ time + 'PaidUtilities' ], climate = client[ time + 'GotClimateControl' ],
+      electricity = client[ time + 'NonHeatElectricity' ], phone = client[ time + 'Phone' ];
 
   /** @todo Owning a home vs. renting vs. homeless should probably be radio buttons */
   return (
     <wrapper>
       <FormHeading>Housing</FormHeading>
 
-      <MassiveToggle id={ time + 'Homeless' } value={ wasHomeless } storeBoolean={ storeBool }
+      <MassiveToggle id={ time + 'Homeless' } value={ wasHomeless } storeChecked={ storeBool }
           label={'Was the household homeless at the last benefit assessment?'}/>
       { wasHomeless
         ? null
         : <MassiveToggle id={ time + 'Homeowner' } value={ ownedAHome }
-            storeBoolean={ storeBool } label={ 'Did the household own a home?' } />
+            storeChecked={ storeBool } label={ 'Did the household own a home?' } />
       }
       { ownedAHome
         ? <wrapper>
             <IntervalColumnHeadings type={ props.type }/>
-            <CashFlowRow {...merge( sharedProps, {generic: 'RentOrMortgage'} )}> Mortgage </CashFlowRow>
-            <CashFlowRow {...merge( sharedProps, {generic: 'HousingInsurance'} )}> Insurance Costs </CashFlowRow>
-            <CashFlowRow {...merge( sharedProps, {generic: 'PropertyTax'} )}> Property Tax </CashFlowRow>
+            <CashFlowRow {...merge( {generic: 'RentOrMortgage'}, sharedProps )}> Mortgage </CashFlowRow>
+            <CashFlowRow {...merge( {generic: 'HousingInsurance'}, sharedProps )}> Insurance Costs </CashFlowRow>
+            <CashFlowRow {...merge( {generic: 'PropertyTax'}, sharedProps )}> Property Tax </CashFlowRow>
           </wrapper>
         : <wrapper> { wasHomeless
           ? null
           : <wrapper>
             <IntervalColumnHeadings type={ props.type }/>
-            <CashFlowRow {...merge( sharedProps, {generic: 'RentOrMortgage'} )}> Rent </CashFlowRow>
-            <MassiveToggle id={ time + 'PaidUtilities' } value={ utils } storeBoolean={ storeBool }
+            <CashFlowRow {...merge( {generic: 'RentOrMortgage'}, sharedProps )}> Rent </CashFlowRow>
+            <MassiveToggle id={ time + 'PaidUtilities' } value={ utils } storeChecked={ storeBool }
               label={'Did the household pay utilities seperately from the rent?'}/>
-            { !client.previousPaidUtilities
+            { !client[ time + 'PaidUtilities' ]
               ? null
               : <wrapper>
-                  <MassiveToggle id={ time + 'ClimateControl' } value={ climate } storeBoolean={ storeBool }
+                  <MassiveToggle id={ time + 'ClimateControl' } value={ climate } storeChecked={ storeBool }
                     label={'Did the household pay for heating or cooling (e.g. A/C during summer), OR did they receive Fuel Assistance in the 12 months prior to the previous benefit assessment?'}/>
-                  <MassiveToggle id={ time + 'NonHeatElectricity' } value={ electricity } storeBoolean={ storeBool }
+                  <MassiveToggle id={ time + 'NonHeatElectricity' } value={ electricity } storeChecked={ storeBool }
                     label={'Did the household pay for electricity for non-heating purposes?'}/>
-                  <MassiveToggle id={ time + 'Telephone' } value={ phone } storeBoolean={ storeBool }
+                  <MassiveToggle id={ time + 'Phone' } value={ phone } storeChecked={ storeBool }
                     label={'Did the household pay for its own telephone service?'}/>
                 </wrapper>
             } </wrapper>
@@ -106,46 +106,91 @@ const Housing = function ( props ) {
 const ExpensesFormContent = function ( props ) {
 
   var client    = props.client,
-      storeBool = props.props.storeBoolean;
+      truth     = props.props,
+      storeBool = truth.storeChecked,
+      time      = 'previous', type = 'expense';
 
-  var needsMedical = client.previousDisabledOrElderlyMember;
+  var needsHandicappedAssistance = client[ time + 'GettingHandicappedAssistance' ]
+    || client[ time + 'DisabledOrElderlyHeadOrSpouse' ]
+    || client[ time + 'HandicappedHeadOrSpouse' ];
 
   var time = 'previous', type = 'expense';
   var sharedProps = { client: client, type: type, time: time,
-                    storeComplex: props.props.storeComplex };
+                    storeComplex: truth.storeComplex };
 
+  /** @todo Determine if client can only enter amounts not covered by other programs
+  * for childcare expenses (does money from those programs count as income?). */
   return (
     <wrapper className='field-aligner two-column'>
 
-      <FormHeading subheading = {'(a dependant is a child or disabled adult)'}>
-        Dependent Monthly Care</FormHeading>
-      <IntervalColumnHeadings type={type}/>
-      <CashFlowRow {...merge( sharedProps, {generic: 'DirectCareCosts'} )}> Direct Care Costs </CashFlowRow>
-      <CashFlowRow {...merge( sharedProps, {generic: 'BeforeAndAfterSchoolPrograms'} )}> Before- and After-School Programs </CashFlowRow>
-      <CashFlowRow {...merge( sharedProps, {generic: 'TransportationCosts'} )}> Transportation Costs </CashFlowRow>
-      <CashFlowRow {...merge( sharedProps, {generic: 'OtherDependentCareCosts'} )}> Other </CashFlowRow>
+      <FormHeading>Dependents</FormHeading>
+      <Form.Field inline>
+        <Input
+          className = { type + 'Dependents' }
+          onChange  = { truth.storeComplex }
+          value     = { client[ type + 'Dependents' ] }
+          style     = {}
+          name      = { type + 'Dependents' }
+          id        = { type + 'Dependents' }
+          type = { 'number' } step = { '0.01' } min = { '0' } />
+        <label labelInfo={'Non-head of hold or their spouse that is under 18, disabled, handicapped, and//or a full-time student'}>Number of dependents</label>
+      </Form.Field>
 
-      <Housing props={props.props} time={time} type={type} />
+      <FormHeading subheading = {'A "child" is a person 12 or younger. Don\'t include amounts that are paid for by other benefit programs.\n'}>
+        Child(ren) Monthly Care</FormHeading>
+      <IntervalColumnHeadings type={type}/>
+      <CashFlowRow {...merge( {generic: 'ChildDirectCareCosts'}, sharedProps )}> Direct Care Costs </CashFlowRow>
+      <CashFlowRow {...merge( {generic: 'ChildBeforeAndAfterSchoolCareCosts'}, sharedProps )}> Before- and After-School Care </CashFlowRow>
+      <CashFlowRow {...merge( {generic: 'ChildTransportationCosts'}, sharedProps )}> Transportation Costs </CashFlowRow>
+      <CashFlowRow {...merge( {generic: 'ChildOtherCareCosts'}, sharedProps )}> Other Care </CashFlowRow>
+      <CashFlowRow {...merge( {generic: 'EarnedIncomeBecauseOfChildCare'}, sharedProps )}> <span style={{textDecoration: 'underline'}}>Income</span> made possible by child care expenses </CashFlowRow>
+
+      <FormHeading>Dependent Care</FormHeading>
+      <IntervalColumnHeadings type={type}/>
+      <CashFlowRow {...merge( {generic: 'ChildSupportPaidOut'}, sharedProps )}> LEGALLY OBLIGATED Child support paid out </CashFlowRow>
+
+      <FormHeading subheading = {'"Elderly" refers to someone 62 or older. Don\'t include amounts that are paid for by other benefit programs.\n'}>
+        Elderly or Disabled Monthly Care</FormHeading>
+      <IntervalColumnHeadings type={type}/>
+      <CashFlowRow {...merge( {generic: 'AdultDirectCareCosts'}, sharedProps )}> Direct Care Costs </CashFlowRow>
+      <CashFlowRow {...merge( {generic: 'AdultTransportationCosts'}, sharedProps )}> Transportation Costs </CashFlowRow>
+      <CashFlowRow {...merge( {generic: 'AdultOtherCareCosts'}, sharedProps )}> Other Care </CashFlowRow>
+
+      <Housing props={props.props} time={time} type={type}/>
+
+      <FormHeading>Medical</FormHeading>
+      <MassiveToggle id={ time + 'GettingHandicappedAssistance' } value={ client[ time + 'GettingHandicappedAssistance' ] }
+        storeChecked={ storeBool }
+        label={'Did the resident have handicapped assistance?'}/>
+      <MassiveToggle id={ time + 'DisabledOrElderlyHeadOrSpouse' } value={ client[ time + 'DisabledOrElderlyHeadOrSpouse' ] }
+        storeChecked={ storeBool }
+        label={'Was the head of household or their spouse disabled or at least 62?'}/>{/** @todo Use for rent medical expenses */}
+      <MassiveToggle id={ time + 'HandicappedHeadOrSpouse' } value={ client[ time + 'HandicappedHeadOrSpouse' ] }
+        storeChecked={ storeBool }
+        label={'Was the head of household or their spouse handicapped?'}/>
+      <IntervalColumnHeadings type={type}/>
+      { !needsHandicappedAssistance
+        ? <CashFlowRow {...merge( {generic: 'Medical'}, sharedProps )}> Medical expenses of the individual(s) </CashFlowRow>
+        : <wrapper>
+          <CashFlowRow {...merge( {generic: 'HandicappedAssistance'}, sharedProps )}> Handicapped Assistance </CashFlowRow>
+          <CashFlowRow {...merge( {generic: 'EarnedIncomeBecauseOfAdultCare'}, sharedProps )}> <span style={{textDecoration: 'underline'}}>Income</span> made possible by elderly or disabled care expenses </CashFlowRow>
+          <CashFlowRow {...merge( {generic: 'Medical'}, sharedProps )}> Medical expenses of the individual(s) </CashFlowRow>
+        </wrapper>
+      }
+      {/*{ !client[ time + 'DisabledOrElderlyHeadOrSpouse' ]
+         ? null
+         : <CashFlowRow {...merge( {generic: 'Medical'}, sharedProps )}> Medical expenses for the handicapped individual(s) </CashFlowRow>}
+      {*/}
 
       <FormHeading>Other</FormHeading>
-      <CashFlowRow {...merge( sharedProps, {generic: 'Medical'} )}> Medical </CashFlowRow>
-      <CashFlowRow {...merge( sharedProps, {generic: 'ChildSupportPaidOut'} )}> LEGALLY OBLIGATED Child support paid out </CashFlowRow>
-      <CashFlowRow {...merge( sharedProps, {generic: 'OtherExpenses'} )}> Other Expenses </CashFlowRow>
+      <CashFlowRow {...merge( {generic: 'OtherExpenses'}, sharedProps )}> Other Expenses </CashFlowRow>
 
     </wrapper>
   );
 
 };  // End ExpensesFormContent()
 
-// If medical is only for disabled or elderly
-// <MassiveToggle id={ time + 'DisabledOrElderlyMember' } value={ client.previousDisabledOrElderlyMember }
-//   storeBoolean={ storeBool }
-//   label={'Was any member of the household elderly (age 60+) or receiving a disability-based benefit (e.g. SSI, EAEDC, MassHealth Disability, etc.)'}/>
-// <IntervalColumnHeadings type={type}/>
-// { !needsMedical
-//   ? null
-//   : <CashFlowRow {...merge( sharedProps, {generic: 'Medical'} )}>Medical</CashFlowRow>
-// }
+// 'previous' to 'was'? 'current' to 'is'? Not always
 
 /**
 * @todo Does a medical assistant's payments count as a medical expense?
@@ -163,7 +208,8 @@ const ExpensesFormContent = function ( props ) {
 * @property {object} props.__ - explanation
 * 
 * @returns Component
-*/// `props` is a cloned version of the original props. References broken.
+*/
+// `props` is a cloned version of the original props. References broken.
 const PreviousExpensesStep = function ( props ) {
 
   return (
