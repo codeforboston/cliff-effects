22411bc2e6a7dd8b9599603f062b731ac25672e7
Shameek Poddar <shameek.poddar@gmail.com>
2017-10-10 21:09:44 -0400

    First crack at ConfirmInformation component

--- src/visitPage.js
+++ src/visitPage.js
@@ -54,6 +54,30 @@ const StepBar = (props) => {
   )
 }
 
+
+const ConfirmInformation = (props) => {
+	
+	return (
+		<FormPartsContainer
+			title     = 'Confirm Information'
+			clarifier = ''
+			left      = {{name: 'Previous', func: props.previousStep}}
+			right     = {{name: 'Next', func: props.nextStep}}>
+			  <div>
+				<Header as='span' color='teal'>Current Benefits</Header> 
+				{props.pageState.hasSnap ? "SNAP, ": ""}
+				{props.pageState.hasHousing ? "Section 8, ": ""}
+				{props.pageState.hasMassHealth ? "MassHealth": ""}<br/>
+				<Header as='span' color='teal'>Household Size</Header> {props.pageState.householdSize}<br/>
+				<Header as='span' color='teal'>Household Annual Income</Header> {props.pageState.annualIncome}<br/>
+				<Header as='span' color='teal'>Citizenship Status</Header> {props.pageState.citizenshipStatus == "citizen" ? "US Citizen" : "Resident" }<br/>
+				<Header as='span' color='teal'>MassHealth</Header> {props.pageState.hasMassHealth ? "Yes, has qualifying conditions" : "No qualifying conditions"  }
+			  </div>
+		  
+      </FormPartsContainer>
+	  )
+}
+
 const AlertSidebar = (props) => {
   let massHealthCard = null;
   let snapCard = null;
@@ -380,6 +404,7 @@ class VisitPage extends Component {
       { completed: false, active: false, title: 'Current Income', form: CurrentIncomeStep },
       { completed: false, active: false, title: 'Citizenship', form: CitizenshipStep },
       { completed: false, active: false, title: 'MassHealth', form: HealthStep },
+      { completed: false, active: false, title: 'Confirm Information', form: ConfirmInformation  },
       // { completed: false, active: false, title: 'SNAP', form: SNAPStep },
       // { completed: false, active: false, title: 'Housing', form: HousingStep },
       { completed: false, active: false, title: 'Results', form: Results }
