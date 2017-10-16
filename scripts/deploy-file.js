8b5d606a3d51e461ec19996cf1066ccd521a0e7c
william <mcintoshwilliamh@gmail.com>
2017-10-16 10:52:03 -0400

    script will replace the value of 'homepage' in package.json with 'new value'

---
+++ scripts/deploy-file.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var fileName = '../package.json';
+var file = require(fileName);
+
+file.homepage = "new value";
+
+fs.writeFile(fileName, JSON.stringify(file, null, 2), function (err) {
+  if (err) return console.log(err);
+  // console.log(JSON.stringify(file));
+  console.log('writing to ' + fileName);
+});
