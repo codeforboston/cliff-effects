94197cb7895b8dbf8d710bd711ff3f1158da6a78
william <mcintoshwilliamh@gmail.com>
2017-10-16 13:12:47 -0400

    removed unneeded files from previous attempts

--- scripts/deploy-dev.js
+++
@@ -1,7 +0,0 @@
-var ghpages = require('gh-pages');
-var path = require('path');
-
-// push the dev branch to codeforbostons's gh-pages branch
-ghpages.publish('build', {
-    repo: 'https://github.com/codeforbostn/cliff-effects.git'
-}, callback);
--- scripts/remote-info.js
+++
@@ -1,8 +0,0 @@
-// get remote repositories
-require('simple-git')()
-    .listRemote(['--get-url'], (err, data) => {
-        if (!err) {
-            console.log('Remote url for repository at ' + __dirname + ':');
-            console.log(data);
-        }
-    });
