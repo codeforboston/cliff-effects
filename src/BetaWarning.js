682e310b430682b5c53fbbd5338c14396687565b
knod <knodding@gmail.com>
2017-10-21 08:40:07 -0400

    New wording

--- src/BetaWarning.js
+++ src/BetaWarning.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 
 const BetaWarning = function ( props ) {
-  // The `style` 'fixes' no space in between those two nodes, but whoever works on
-  // this long term might consider a different UI package because that's not great.
   return (
     <Menu.Item>
-      <div style={{ display: 'block' }}><strong>WARNING:</strong>{' '}This tool is a prototype. It should not be used for official purposes.</div>
+      <div>This tool is a prototype and should not be used to make financial decisions.</div>
     </Menu.Item>
   );
 };  // End BetaWarning(<>)
