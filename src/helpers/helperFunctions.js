c81698d7b0d6061d78c040d596d5cee1367f7c3a
knod <knodding@gmail.com>
2017-10-28 14:04:57 -0400

    Reasses state median income percent, though sources unidentified
    On further examination, state median income percent values seem
    to be used by massHealth calcuations, though it's unclear where
    the data came from and massHealth calculations are currently
    only placeholders and will be removed completely soon. Still,
    inquiries will be made.

--- src/helpers/helperFunctions.js
+++ src/helpers/helperFunctions.js
@@ -1,6 +1,5 @@
 import { getLimitBySize } from './get-gov-data.js';
 import { federalPovertyGuidelines } from '../data/federal/federalPovertyGuidelines.js';
-import { stateMedianIncome } from '../data/state/massachusetts/stateIncomeLevels.js';
 
 function percentPovertyLevel(annualIncome, householdSize) {
   var monthlyLimit  = getLimitBySize( federalPovertyGuidelines, householdSize ),
@@ -11,13 +10,28 @@ function percentPovertyLevel(annualIncome, householdSize) {
   return percent; 
 }
 
-// State median income calcs have been removed as they don't seem to be used
-// by anything in here (and from documentation are only used to derive other
-// HUD values). They're also limited to an 8 person household with no ability
-// to calculate other variations. If you want to get info about them, look at
+// State median income calcs should been removed as they only seem to be used
+// by Mass Health and their source hasn't been found. Actual 'state median income
+// limits' values seem, from linked documentation, to only be used to derive HUD
+// values). In documentation, they're also limited to an 8 person household with no
+// ability to calculate other variations. If you want to get info about them, look at
 // https://docs.google.com/document/d/1DRNm1TLP31s_yDdsH8IDoRV7_KjjJ46NyAaZOgLoQmY/edit#
 // and https://www.huduser.gov/portal/datasets/il.html#2017_data
+function percentStateMedianIncome(annualIncome, householdSize) {
+  var smi = new Map();
+  smi.set(1,56668);
+  smi.set(2,74105);
+  smi.set(3,91542);
+  smi.set(4,108978);
+  smi.set(5,126415);
+  smi.set(6,143852);
+  smi.set(7,147120);
+  smi.set(8,150390);
+
+  return (100*(annualIncome)/(smi.get(householdSize))); 
+}
+
 
 export {
-  percentPovertyLevel
+  percentPovertyLevel, percentStateMedianIncome
 };
