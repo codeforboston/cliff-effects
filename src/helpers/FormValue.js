2ea5a544e61a1f832f68f6666b28cfba3f3d1c66
knod <knodding@gmail.com>
2017-11-09 17:06:36 -0500

    Converts back to transforming, fixes 'changed' tracker

--- src/helpers/FormValue.js
+++ src/helpers/FormValue.js
@@ -4,7 +4,7 @@
  * - Propagating current values to future values if they
  * haven't been manipulated by the user
  * - Making this instance serializable
- * - Sending values to be validated
+ * - Making sure only valid values are saved.
  * 
  * All values stored in a FormValue instance should be valid
  * 
@@ -13,8 +13,8 @@
  * @class
  * @param {string} name - the name of the client property
  * @param {any} [val] - the initial value
- * @param {function} [validate] - returns whether the new value
- * is valid. Will be given the new value, the current value,
+ * @param {function} [makeValid] - returns a valid value no
+ * matter what. Will be given the new value, the current value,
  * the string 'current' or 'future', and the `FormValue` instance.
  * 
  * @returns FormValue
@@ -22,13 +22,13 @@
  */
 class FormValue {
 
-  constructor ( name, val = null, validate ) {
+  constructor ( name, val = null, makeValid ) {
 
     var fVal = this;
     
     fVal.name = name;
     fVal._futureWasChanged = false;  // Handled internally
-    fVal.validate = validate || function () { return true; };
+    fVal.makeValid = makeValid || function ( newVal ) { return newVal; };
     
     fVal.setCurrent( val );
 
@@ -39,10 +39,8 @@ class FormValue {
 
     var fVal = this;
 
-    // Transformer would be less verbose, but open to devs' complications
-    if ( fVal.validate( val, fVal.current, 'current', fVal ) ) {
-      fVal.current = val;
-    }
+    // Transformer needed for parseInt and such
+    fVal.current = fVal.makeValid( val, fVal.current, 'current', fVal );
 
     if ( !fVal._futureWasChanged ) {
       fVal.setFuture( val, true );
@@ -53,14 +51,15 @@ class FormValue {
   }  // End FormValue.setCurrent()
 
 
-  setFuture ( val, fromCurrent ) {
+  setFuture ( val, propagating ) {
     
     var fVal = this;
-
-    this._futureWasChanged = !fromCurrent;
-    if ( fVal.validate( val, fVal.future, 'future', fVal ) ) {
-      fVal.future = val;
+    // If it hasn't already been changed by the user...
+    if ( !fVal._futureWasChanged ) {
+      // ...mark it as changed if needed
+      fVal._futureWasChanged = !propagating;
     }
+    fVal.future = fVal.makeValid( val, fVal.future, 'future', fVal );
     
     return fVal;  // Return valid value instead?
 
@@ -83,7 +82,7 @@ class FormValue {
   // Is this how deserialization works?
   static deserialize ( obj ) {
 
-    var fVal = new FormValue( obj.name, obj.current, obj.validate );
+    var fVal = new FormValue( obj.name, obj.current, obj.makeValid );
     if ( obj.futureWasChanged ) {
       fVal.setFuture( obj.future );
     }
