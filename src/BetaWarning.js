1d91372d7eefbc675bee40a3fbe77122686625c5
Sasha Goldberg <sashamaryl@gmail.com>
2017-11-20 14:31:17 -0500

    fixes issue 186 -- makes disclaimer more evident

--- src/BetaWarning.js
+++ src/BetaWarning.js
@@ -4,7 +4,7 @@ import { Menu } from 'semantic-ui-react';
 const BetaWarning = function ( props ) {
   return (
     <Menu.Item>
-      <div>This tool is a prototype and should not be used to make financial decisions.</div>
+      <div><strong>This tool is a prototype and should not be used to make financial decisions.</strong></div>
     </Menu.Item>
   );
 };  // End BetaWarning(<>)
