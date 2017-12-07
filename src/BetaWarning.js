a8c8763db9b8a93b817821dc7577e141bdd7bf01
Andrew Cunningham <wacii@mail.com>
2017-12-07 12:55:53 -0500

    Shorten beta warning on tablet

--- src/BetaWarning.js
+++ src/BetaWarning.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Responsive } from 'semantic-ui-react';
 
 const BetaWarning = function ( props ) {
   return (
     <Menu.Item>
-      <div><strong>This tool is a prototype and should not be used to make financial decisions.</strong></div>
+      <Responsive as='strong' {...Responsive.onlyTablet}>
+        This tool is a prototype.
+      </Responsive>
+      <Responsive as='strong' {...Responsive.onlyComputer}>
+        This tool is a prototype and should not be used to make financial decisions.
+      </Responsive>
     </Menu.Item>
   );
 };  // End BetaWarning(<>)
