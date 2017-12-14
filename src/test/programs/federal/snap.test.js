ef0fb8029dfc7813373d1a7882ca2b62183522e7
Don Blair <donblair10@gmail.com>
2017-12-14 16:08:34 -0500

    Update snap tests for row 6 & 7

--- src/test/programs/federal/snap.test.js
+++ src/test/programs/federal/snap.test.js
@@ -45,9 +45,8 @@ describe('getSNAPBenefits', () => {
   describe('row6', () => {
     it('Should calculate the correct current benefits', () => {
       let client = sampleClients.row6;
-      // Sample data result didn't match. Missing data?
       // expect(getSNAPBenefits(client, 'current')).toBeCloseTo(client.current.expectedSNAP, 4);
-      // Current result
+      // Project Hope: 192, Excel: 352, Ours: 352
       expect(getSNAPBenefits(client, 'current')).toBeCloseTo(352, 4);
     });
   });
@@ -55,9 +54,8 @@ describe('getSNAPBenefits', () => {
   describe('row7', () => {
     it('Should calculate the correct current benefits', () => {
       let client = sampleClients.row7;
-      // Sample data result didn't match. Missing data?
       // expect(getSNAPBenefits(client, 'current')).toBeCloseTo(client.current.expectedSNAP, 4);
-      // Current result
+      // Project Hope: 20, Excel: 0, Ours: 0
       expect(getSNAPBenefits(client, 'current')).toBeCloseTo(0, 4);
     });
   });
