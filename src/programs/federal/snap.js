a60f1c45ee23077dbcefc4e588747e76fef445e3
knod <knodding@gmail.com>
2017-12-14 12:37:36 -0500

    Simple rearrangement and grouping

--- src/programs/federal/snap.js
+++ src/programs/federal/snap.js
@@ -51,17 +51,35 @@ const getSNAPBenefits = function ( client, timeframe ) {
 }; // End getSNAPBenefits()
 
 
-
+// ======================
+// BENEFIT HELPER FUNCTIONS
 var SNAPhelpers = {},
     hlp         = SNAPhelpers;
 
-//GROSS INCOME TEST
+
+// ======================
+// HOUSEHOLD/HOUSEHOLD MEMBERS
+hlp.householdSize = function ( client ) {
+  return client.household.length;
+};
+
 hlp.isElderlyOrDisabled = function ( member ) {
   return member.age >= 60 || isDisabled( member );
 };
 
 hlp.hasDisabledOrElderlyMember = function (client) {
   return getEveryMemberOfHousehold( client, hlp.isElderlyOrDisabled ).length > 0;
+};
+
+hlp.isDependentOver12 = function ( member ) {
+  return (!isUnder13( member ) && member.age <= 18) || isDisabled( member );
+};
+
+
+// ======================
+//GROSS INCOME
+hlp.getChildSupportPaid = function (client) {
+  return client.childSupportPaidOut;
 };
 
 hlp.getAdjustedGross = function (client) {
@@ -92,71 +110,9 @@ hlp.getGrossIncomeTestResult = function (client) {
   return isPassGrossIncomeTest;
 };
 
-// INCOME DEDUCTIONS
-hlp.getStandardDeduction = function (client) {
-  return getYearlyLimitBySize(SNAPData.STANDARD_DEDUCTIONS, hlp.householdSize( client ));
-};
 
-hlp.getEarnedIncomeDeduction = function (client) {
-  var totalMonthlyEarned = client.earned;
-  return totalMonthlyEarned * SNAPData.PERCENT_GROSS_MONTHLY_EARNED;
-};
-
-hlp.getMedicalDeduction = function (client) {
-  var medicalDeduce = 0;
-
-  if ( hlp.hasDisabledOrElderlyMember(client) === true ) {
-    /** @todo Add disabledAssistance too. Also, otherMedical? */
-    var medicalExpenses = client.disabledMedical;
-    if ((medicalExpenses >= SNAPData.MIN_MEDICAL_EXPENSES) && (medicalExpenses <= SNAPData.MAX_MEDICAL_EXPENSES)) {
-      medicalDeduce = SNAPData.STANDARD_MEDICAL_DEDUCTION;
-
-    } else if (medicalExpenses >= SNAPData.MAX_MEDICAL_EXPENSES++) {
-      medicalDeduce = medicalExpenses - SNAPData.MIN_MEDICAL_EXPENSES;
-
-    }
-  }  // end if has disabled or elderly
-
-  return medicalDeduce;
-};
-
-hlp.isDependentOver12 = function ( member ) {
-  return (!isUnder13( member ) && member.age <= 18) || isDisabled( member );
-};
-
-hlp.getDependentCareDeduction = function (client) {
-
-  var dependentCare = 0;
-
-  /** @todo Adopt https://github.com/codeforboston/cliff-effects/issues/264
-   *     model for all these 'kinds' of 'if' situations. If possible. */
-  if ( getUnder13OfHousehold( client ).length > 0 ) {
-    dependentCare += sumProps( client, UNDER13_CARE_EXPENSES );
-  }
-
-  if ( getEveryMemberOfHousehold( client, hlp.isDependentOver12 ).length > 0 ) {
-    dependentCare += sumProps( client, OVER12_CARE_EXPENSES );
-  }
-
-  return dependentCare;
-};
-
-hlp.getChildSupportPaid = function (client) {
-  return client.childSupportPaidOut;
-};
-
-hlp.getAdjustedIncome = function (client) {
-  var adjustedGross           = hlp.getAdjustedGross(client),
-      standardDeduction       = hlp.getStandardDeduction(client),
-      earnedIncomeDeduction   = hlp.getEarnedIncomeDeduction(client),
-      medicalDeduction        = hlp.getMedicalDeduction(client),
-      dependentCareDeduction  = hlp.getDependentCareDeduction(client);
-
-  var adjustedIncome = adjustedGross - standardDeduction - earnedIncomeDeduction - medicalDeduction - dependentCareDeduction;
-  return Math.max( 0, adjustedIncome );
-};
-
-// EXPENSE DEDUCTIONS
+// ======================
+// SHELTER
 hlp.isHomeless = function(client ) {
   // Worth abstracting, used a few places and may change
   return client.shelter === 'homeless';
@@ -209,8 +165,51 @@ hlp.getTotalshelterCost = function (client) {
   return shelterDeduction + utilityDeductions;
 };
 
-hlp.getHalfAdjustedIncome = function(client) {
-  return hlp.getAdjustedIncome(client) * 0.50;
+
+// ======================
+// INCOME DEDUCTIONS
+hlp.getStandardDeduction = function (client) {
+  return getYearlyLimitBySize(SNAPData.STANDARD_DEDUCTIONS, hlp.householdSize( client ));
+};
+
+hlp.getEarnedIncomeDeduction = function (client) {
+  var totalMonthlyEarned = client.earned;
+  return totalMonthlyEarned * SNAPData.PERCENT_GROSS_MONTHLY_EARNED;
+};
+
+hlp.getMedicalDeduction = function (client) {
+  var medicalDeduce = 0;
+
+  if ( hlp.hasDisabledOrElderlyMember(client) === true ) {
+    /** @todo Add disabledAssistance too. Also, otherMedical? */
+    var medicalExpenses = client.disabledMedical;
+    if ((medicalExpenses >= SNAPData.MIN_MEDICAL_EXPENSES) && (medicalExpenses <= SNAPData.MAX_MEDICAL_EXPENSES)) {
+      medicalDeduce = SNAPData.STANDARD_MEDICAL_DEDUCTION;
+
+    } else if (medicalExpenses >= SNAPData.MAX_MEDICAL_EXPENSES++) {
+      medicalDeduce = medicalExpenses - SNAPData.MIN_MEDICAL_EXPENSES;
+
+    }
+  }  // end if has disabled or elderly
+
+  return medicalDeduce;
+};
+
+hlp.getDependentCareDeduction = function (client) {
+
+  var dependentCare = 0;
+
+  /** @todo Adopt https://github.com/codeforboston/cliff-effects/issues/264
+   *     model for all these 'kinds' of 'if' situations. If possible. */
+  if ( getUnder13OfHousehold( client ).length > 0 ) {
+    dependentCare += sumProps( client, UNDER13_CARE_EXPENSES );
+  }
+
+  if ( getEveryMemberOfHousehold( client, hlp.isDependentOver12 ).length > 0 ) {
+    dependentCare += sumProps( client, OVER12_CARE_EXPENSES );
+  }
+
+  return dependentCare;
 };
 
 hlp.getRawShelterDeduction = function(client) {
@@ -240,6 +239,26 @@ hlp.getShelterDeduction = function(client) {
 hlp.getHomelessDeduction = function(client) {
   if ( hlp.isHomeless(client) ) { return SNAPData.HOMELESS_DEDUCTION; }
   else { return 0; }
+};
+
+
+// ======================
+// NET INCOME
+hlp.getAdjustedIncome = function (client) {
+  var adjustedGross           = hlp.getAdjustedGross(client),
+      standardDeduction       = hlp.getStandardDeduction(client),
+      earnedIncomeDeduction   = hlp.getEarnedIncomeDeduction(client),
+      medicalDeduction        = hlp.getMedicalDeduction(client),
+      dependentCareDeduction  = hlp.getDependentCareDeduction(client);
+
+  var adjustedIncome = adjustedGross - standardDeduction - earnedIncomeDeduction - medicalDeduction - dependentCareDeduction;
+  return Math.max( 0, adjustedIncome );
+};
+
+// EXPENSE DEDUCTIONS
+
+hlp.getHalfAdjustedIncome = function(client) {
+  return hlp.getAdjustedIncome(client) * 0.50;
 };
 
 // NET INCOME CALCULATION
@@ -290,10 +309,6 @@ hlp.getNetIncomeTestResult = function(client ) {
     return false;
   }
 
-};
-
-hlp.householdSize = function ( client ) {
-  return client.household.length;
 };
 
 /** NOTE: Bay State CAP not included as this prototype only deals with
