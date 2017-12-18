aed7fbf83f054def3ef442091aae01fd083dd9e1
knod <knodding@gmail.com>
2017-12-18 10:03:49 -0500

    Adds `.isHomeless()` tests, cleans up old tests

--- src/test/programs/federal/snap/unit.test.js
+++ src/test/programs/federal/snap/unit.test.js
@@ -29,17 +29,17 @@ describe('SNAPhelpers', () => {
   // `SNAPhelpers.householdSize()`
   describe('`.householdSize( timeClient )` given a time-restricted client object should get household size of', () => {
     it('1', () => {
-      expect(SNAPhelpers.householdSize( defaultCurrent, 'current' )).toEqual(1);
+      expect(SNAPhelpers.householdSize( defaultCurrent )).toEqual(1);
     });
     it('5', () => {
       let current = cloneDeep( defaultCurrent );
       addNumMembers( 4, current.household );
-      expect(SNAPhelpers.householdSize( current, 'current' )).toEqual(5);
+      expect(SNAPhelpers.householdSize( current )).toEqual(5);
     });
     it('15', () => {
       let current = cloneDeep( defaultCurrent );
       addNumMembers( 14, current.household );
-      expect(SNAPhelpers.householdSize( current, 'current' )).toEqual(15);
+      expect(SNAPhelpers.householdSize( current )).toEqual(15);
     });
   });
 
@@ -99,7 +99,7 @@ describe('SNAPhelpers', () => {
 
 
   // `SNAPhelpers.hasDisabledOrElderlyMember()`
-  describe('`.isElderlyOrDisabled( household )` given a time-constrained client with a household containing', () => {
+  describe('`.isElderlyOrDisabled( timeClient )` given a time-constrained client with a household containing', () => {
     let current;
     beforeEach(() => { current = cloneDeep( defaultCurrent ); });
 
@@ -148,7 +148,7 @@ describe('SNAPhelpers', () => {
 
 
   // `SNAPhelpers.hasDependentsOver12()`
-  describe('`.hasDependentsOver12( client )` given a time-constrained client with a household containing', () => {
+  describe('`.hasDependentsOver12( timeClient )` given a time-constrained client with a household containing', () => {
     let current;
     beforeEach(() => { current = cloneDeep( defaultCurrent ); });
 
@@ -214,10 +214,84 @@ describe('SNAPhelpers', () => {
     });
   });
 
+
+  // `SNAPhelpers.getChildSupportPaid()`
+  describe('`.getChildSupportPaid( timeClient )` given a time-constrained client paying', () => {
+    let current;
+    beforeEach(() => { current = cloneDeep( defaultCurrent ); });
+
+    describe('no ', () => {
+      it('that is not disabled and under 60 should return false', () => {
+        expect(SNAPhelpers.hasDisabledOrElderlyMember( current )).toBe(false);
+      });
+      it('that is disabled should return true', () => {
+        current.household[0].m_disabled = true;
+        expect(SNAPhelpers.hasDisabledOrElderlyMember( current )).toBe(true);
+      });
+      it('that is 60 should return true', () => {
+        current.household[0].m_age = 60;
+        expect(SNAPhelpers.hasDisabledOrElderlyMember( current )).toBe(true);
+      });
+    });
+    describe('a spouse', () => {
+      it('that is not disabled and under 60 should return false', () => {
+        current.household.push({ m_age: 30, m_role: 'spouse', m_disabled: false });
+        expect(SNAPhelpers.hasDisabledOrElderlyMember( current )).toBe(false);
+      });
+      it('that is disabled should return true', () => {
+        current.household.push({ m_age: 30, m_role: 'spouse', m_disabled: true });
+        expect(SNAPhelpers.hasDisabledOrElderlyMember( current )).toBe(true);
+      });
+      it('that is 60 should return true', () => {
+        current.household.push({ m_age: 60, m_role: 'spouse', m_disabled: false });
+        expect(SNAPhelpers.hasDisabledOrElderlyMember( current )).toBe(true);
+      });
+    });
+    describe('a regular member', () => {
+      it('that is not disabled and under 60 should return false', () => {
+        current.household.push({ m_age: 30, m_role: 'member', m_disabled: false });
+        expect(SNAPhelpers.hasDisabledOrElderlyMember( current )).toBe(false);
+      });
+      it('that is disabled should return true', () => {
+        current.household.push({ m_age: 30, m_role: 'member', m_disabled: true });
+        expect(SNAPhelpers.hasDisabledOrElderlyMember( current )).toBe(true);
+      });
+      it('that is 60 should return true', () => {
+        current.household.push({ m_age: 60, m_role: 'member', m_disabled: false });
+        expect(SNAPhelpers.hasDisabledOrElderlyMember( current )).toBe(true);
+      });
+    });
+  });
+
+
   // `SNAPhelpers.getAdjustedGross()`
   // `SNAPhelpers.getPovertyGrossIncomeLevel()`
   // `SNAPhelpers.getGrossIncomeTestResult()`
+
+
   // `SNAPhelpers.isHomeless()`
+  describe('`.isHomeless( timeClient )` given a time-restricted client object', () => {
+    let current;
+    beforeEach(() => { current = cloneDeep( defaultCurrent ); });
+
+    it('that is "homeless", shoud return true', () => {
+      expect(SNAPhelpers.isHomeless( current )).toBe(true);
+    });
+    it('that is a "renter", should return false', () => {
+      current.shelter = 'renter';
+      expect(SNAPhelpers.isHomeless( current )).toEqual(false);
+    });
+    it('that is a "voucher", should return false', () => {
+      current.shelter = 'voucher';
+      expect(SNAPhelpers.isHomeless( current )).toEqual(false);
+    });
+    it('that is a "homeowner", should return false', () => {
+      current.shelter = 'homeowner';
+      expect(SNAPhelpers.isHomeless( current )).toEqual(false);
+    });
+  });
+
+
   // `SNAPhelpers.getNonUtilityCosts()`
   // `SNAPhelpers.getUtilityCostByBracket()`
   // `SNAPhelpers.getTotalshelterCost()`
