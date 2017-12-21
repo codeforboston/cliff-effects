6f3b49cc7d6560b3c2bc9bf114b48f55603499eb
Stephen Chin <scapp281@gmail.com>
2017-12-21 16:47:21 -0500

    test getTimeSetter.js

---
+++ src/test/utils/getTimeSetter.test.js
@@ -0,0 +1,19 @@
+import { getTimeSetter } from '../../utils/getTimeSetter';
+
+describe('getTimeSetter', () => {
+  var data = {};
+
+  const fakeCallback = function ( evnt, data ) {};
+
+  var returnedFunc= getTimeSetter('current', fakeCallback);
+  returnedFunc( {}, data );
+
+  it('does return a function', () => {
+    expect(typeof(returnedFunc) === 'function').toBe(true);
+  });
+
+  it('adds time property to data', () => {
+    expect(data.hasOwnProperty('time')).toBe(true);
+  });
+
+});
