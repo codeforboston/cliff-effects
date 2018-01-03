0ae00c18e2378b3cbbc3d0a49c1a59ef94f9f3a0
Andrew Cunningham <wacii@mail.com>
2018-01-03 18:08:04 -0500

    Add DownloadFileAs component

---
+++ src/components/DownloadFileAs.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import DownloadFile from './DownloadFile';
+
+/**
+ * Download file with editable name.
+ * @extends React.Component
+ * 
+ * @param props {object}
+ * @param props.children {string} - Link label
+ * @param props.defaultFilename {string} - Initial value of filename
+ * @param props.url {string} - URL to file
+ */
+class DownloadFileAs extends React.Component {
+  updateFilename = filename => this.setState({ filename: filename });
+
+  constructor(props) {
+    super(props);
+    this.state = { filename: props.defaultFilename };
+  }
+
+  render() {
+    const { children, url } = this.props;
+    const { filename } = this.state;
+
+    return (
+      <div>
+        <input onChange={this.updateFilename} value={filename} />
+        <DownloadFile url={url} filename={filename}>
+          {children}
+        </DownloadFile>
+      </div>
+    );
+  }
+}
+
+export default DownloadFileAs;
