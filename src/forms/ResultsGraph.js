55d20a44563b3ea82fceba032f327f3c45524455
Andrew Cunningham <wacii@mail.com>
2018-01-22 15:42:22 -0500

    Add stacked area chart to Results

--- src/forms/ResultsGraph.js
+++ src/forms/ResultsGraph.js
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Bar, Line } from 'react-chartjs-2';
 
 // Logic
 import { getSNAPBenefits } from '../programs/federal/snap';
@@ -39,29 +39,29 @@ const ResultsGraph = (props) => {
 
   // TAFDC color? "rgba(206, 125, 61, 1)"
 
-  var data = {
-    labels: xRange,
-    datasets: [
-      {
-        label: "SNAP",
-        backgroundColor: "rgba(101, 47, 138, 1)",
-        data: snapData
-      },
-      {
-        label: "Section 8 Housing",
-        backgroundColor: "rgba(206, 203, 61, 1)",
-        data: housingData
-      },
-    ]
-  };
-
-  var options = {
-    title: {
-      display: true,
-        text: 'Benefit Eligibility for Household Size ' +
-                props.client.householdSize
+  const barProps = {
+    data: {
+      labels: xRange,
+      datasets: [
+        {
+          label: "SNAP",
+          backgroundColor: "rgba(101, 47, 138, 1)",
+          data: snapData
+        },
+        {
+          label: "Section 8 Housing",
+          backgroundColor: "rgba(206, 203, 61, 1)",
+          data: housingData
+        },
+      ]
     },
-    scales: {
+    options: {
+      title: {
+        display: true,
+        text: 'Benefit Eligibility for Household Size ' +
+          props.client.householdSize
+      },
+      scales: {
         yAxes: [{
           stacked: true,
           scaleLabel: {
@@ -69,14 +69,14 @@ const ResultsGraph = (props) => {
               labelString: 'Benefit Value ($)'
           },
           ticks: {
-              beginAtZero: true,
-              /*
-               * function to add $ and 1,000s separators to graph axes
-               * we are using chart.js v2.7 so it requires a callback function
-               */
-              callback: function(label) {
-                  return label.toLocaleString("en-US");
-              }
+            beginAtZero: true,
+            /*
+              * function to add $ and 1,000s separators to graph axes
+              * we are using chart.js v2.7 so it requires a callback function
+              */
+            callback: function(label) {
+              return label.toLocaleString("en-US");
+            }
           }
         }],
         xAxes: [{
@@ -85,58 +85,142 @@ const ResultsGraph = (props) => {
             display: true,
               labelString: 'Annual Income ($)'
           },
-            ticks: {
-              callback: function(label) {
-                  return label.toLocaleString("en-US");
-              }
+          ticks: {
+            callback: function(label) {
+              return label.toLocaleString("en-US");
             }
+          }
         }]
-    },
-      /*
-       * default tooltip for chart.js 2.0+  when unspecified looks like:
-       *
-       * options: {
-       *   tooltips: {
-       *       callbacks: {
-       *           label: function(tooltipItem, data) {
-       *               return tooltipItem.yLabel;
-       *           }
-       *       }
-       *   }
-       * }
-       *
-       */
-    tooltips: {
+      },
+        /*
+         * default tooltip for chart.js 2.0+  when unspecified looks like:
+         *
+         * options: {
+         *   tooltips: {
+         *       callbacks: {
+         *           label: function(tooltipItem, data) {
+         *               return tooltipItem.yLabel;
+         *           }
+         *       }
+         *   }
+         * }
+         *
+         */
+      tooltips: {
         callbacks: {
-            // format the title of the tooltips to be in USD
-            title: function(tooltipItems, data) {
-                return data.labels[tooltipItems[0].index].toLocaleString("en-US",
-                    {style:"currency",currency:"USD"}).replace('.00','');
-            },
-            /*
-             * to add number formatting to the tooltips. returns the data label
-             * + currency format
-             * from https://github.com/chartjs/Chart.js/issues/2386
-             */
-            label: function(tooltipItem, data) {
-                return data.datasets[tooltipItem.datasetIndex].label + ": " +
-                    tooltipItem.yLabel.toLocaleString("en-US",{style:"currency",
-                        currency:"USD"}).replace('.00','');
-            }
+          // format the title of the tooltips to be in USD
+          title: function(tooltipItems, data) {
+            return data.labels[tooltipItems[0].index].toLocaleString(
+              "en-US",
+              {style:"currency",currency:"USD"}
+            ).replace('.00','');
+          },
+          /*
+            * to add number formatting to the tooltips. returns the data label
+            * + currency format
+            * from https://github.com/chartjs/Chart.js/issues/2386
+            */
+          label: function(tooltipItem, data) {
+            return data.datasets[tooltipItem.datasetIndex].label + ": " +
+              tooltipItem.yLabel.toLocaleString(
+                "en-US",
+                {style:"currency", currency:"USD"}
+              ).replace('.00','');
+          }
         }
+      }
     }
   };
 
-  // return (
-  //   <div className = 'result-page'>
-  //     <FormPartsContainer
-  //       title     = {'Results'}
-  //       left      = {{ name: 'Go Back', func: props.previousStep }}
-  //       right     = {{ name: 'Save Results', func: () => props.saveForm(false) }}
-  //        <div> <Line data={data} options={options} /> </div>
-  //     </FormPartsContainer>
-  //   </div>
-  // )
+  const lineProps = {
+    data: {
+      labels: xRange.slice(0, 50),
+      datasets: [
+        {
+          label: "Income",
+          backgroundColor: "rgba(60, 136, 206, 0.55)",
+          data: xRange.slice(0, 50),
+          fill: "origin"
+        },
+        {
+          label: "SNAP",
+          backgroundColor: "rgba(101, 47, 138, 0.55)",
+          data: snapData.slice(0, 50)
+        },
+        {
+          label: "Section 8 Housing",
+          backgroundColor: "rgba(206, 203, 61, 0.55)",
+          data: housingData.slice(0, 50)
+        },
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Benefit Eligibility for Household Size ' +
+          props.client.householdSize
+      },
+      elements: {
+        line: {
+          fill: '-1'
+        },
+        point: {
+          radius: 0,
+          hitRadius: 10,
+          hoverRadius: 10
+        }
+      },
+      scales: {
+        yAxes: [{
+          stacked: true,
+          scaleLabel: {
+            display: true,
+              labelString: 'Benefit Value ($)'
+          },
+          ticks: {
+            beginAtZero: true,
+            /*
+              * function to add $ and 1,000s separators to graph axes
+              * we are using chart.js v2.7 so it requires a callback function
+              */
+            callback: function(label) {
+                return label.toLocaleString("en-US");
+            }
+          }
+        }],
+        xAxes: [{
+          stacked: true,
+          scaleLabel: {
+            display: true,
+              labelString: 'Annual Income ($)'
+          },
+          ticks: {
+            callback: function(label) {
+                return label.toLocaleString("en-US");
+            }
+          }
+        }]
+      },
+      tooltips: {
+        callbacks: {
+          title: function(tooltipItems, data) {
+            const { index } = tooltipItems[0];
+            return _.sumBy(data.datasets, dataset => dataset.data[index]).toLocaleString(
+              "en-US",
+              {style:"currency",currency:"USD"}
+            ).replace('.00','');
+          },
+          label: function(tooltipItem, data) {
+            return data.datasets[tooltipItem.datasetIndex].label + ": " +
+              tooltipItem.yLabel.toLocaleString(
+                "en-US",
+                {style:"currency", currency:"USD"}
+              ).replace('.00','');
+          }
+        }
+      }
+    }
+  }
 
   // Non-saving version for first prototype testing
   return (
@@ -146,7 +230,10 @@ const ResultsGraph = (props) => {
         left      = {{ name: 'Go Back', func: props.previousStep }}
         right      = {{ name: 'Reset', func: props.resetClient }}
       >
-         <div> <Bar data={data} options={options} /> </div>
+         <div>
+           <Bar {...barProps} />
+           <Line {...lineProps} />
+          </div>
       </FormPartsContainer>
     </div>
   )
