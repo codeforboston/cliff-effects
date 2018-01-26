bc22b6230f40c78a860001e1dac5fcc0ee8a9c67
Andrew Cunningham <wacii@mail.com>
2018-01-26 15:32:16 -0500

    Fix Predictions snapshot test
    The react-test-renderer calls componentDidMount but provides no
    underlying DOM, which creates a situation unsupported by chartjs.
    Instead enzyme shallow renderer was used to avoid the problem.

--- src/test/forms/Predictions.test.js
+++ src/test/forms/Predictions.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { configure } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { PredictionsStep } from '../../forms/Predictions';
@@ -15,7 +14,7 @@ test('Prediction component renders as snapshot correctly', () => {
   const setClientProperty = jest.fn();
   const changeClient = jest.fn();
   const saveForm = jest.fn();
-  const rendered = renderer.create(
+  const wrapper = shallow(
     <PredictionsStep
       currentStep={1}
       client={CLIENT_DEFAULTS}
@@ -26,5 +25,5 @@ test('Prediction component renders as snapshot correctly', () => {
       saveForm={saveForm}
      />
   );
-  expect(rendered.toJSON()).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
