049b08aa4296382b27febc8c89463e31e9071ceb
Miguel Cepeda <miguel@MacBook-Pro.local>
2018-01-26 20:38:22 -0500

    Create component to contain page contents

---
+++ src/components/PageLayout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Grid,
+  Segment,
+} from 'semantic-ui-react';
+
+// import { AboutContent } from '../components/AboutContent';
+
+const PageLayout = (props) => {
+  return (
+    <div>
+      <Segment style={{ padding: '8em 0em' }} vertical>
+        <Grid container stackable verticalAlign='middle'>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              {props.children}
+            </Grid.Column>
+            <Grid.Column floated='right' width={6}>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    </div>
+  )
+}
+
+export { PageLayout };
