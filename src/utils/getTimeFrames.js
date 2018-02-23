d1ee7584747b369f4f5a4c110be68498f3c7a96e
Miguel Cepeda <miguel@MacBook-Pro.local>
2018-02-23 17:26:12 -0500

    Consolidate current and future program and income values on Predictions page

---
+++ src/utils/getTimeFrames.js
@@ -0,0 +1,19 @@
+const getBenefitTimeFrames = function ( client, benefitCheck, benefitsFunc ) {
+    if (client.current[benefitCheck]) {
+        return {
+            benefitCurrent: Math.round( benefitsFunc ( client, 'current' ) * 12 ),
+            benefitFuture: Math.round( benefitsFunc ( client, 'future' ) * 12 )
+        }
+    } else {
+        return 0;
+    }
+};
+
+const getIncomeTimeFrames = function( client ) {
+    return {
+        incomeCurrent: Math.round( client.current.earned * 12 ),
+        incomeFuture: Math.round( client.future.earned * 12 )
+    }
+};
+
+export { getBenefitTimeFrames, getIncomeTimeFrames };
