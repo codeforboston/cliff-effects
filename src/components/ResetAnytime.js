126d5db610df4ebb95dc9d7f1c393f12cd6d6d21
Thescribe83 <Therrienjr.david@yahoo.com>
2018-04-14 10:12:03 -0400

    Changed text of Reset Button to New Client in Viewport, also added a mouseover

--- src/components/ResetAnytime.js
+++ src/components/ResetAnytime.js
@@ -7,7 +7,7 @@ const ResetAnytime = function ( props ) {
   return (
     <div>
       <Button onClick={props.resetClient} type='button' color='teal' size='medium' className={'fixed rotate'} id={'reset_fixed'}>
-        Reset
+        New Client
       </Button>
     </div>
   );
