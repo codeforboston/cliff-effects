614c2f96fdd65620cd46688fe4995982b96a3ad0
knod <knodding@gmail.com>
2018-05-03 22:08:28 -0400

    Converts localization to objects and does some validation

--- src/utils/getTranslate.js
+++ src/utils/getTranslate.js
@@ -1,25 +1,76 @@
 /** Returns a translator based on the language given */
-import { localizations } from '../localization/all';
+import { get } from 'lodash';
 
-const getTranslate = function (appLanguage) {
+// DATA
+import { localizations } from '../localization/all';
+var english = localizations.en;  // unforunately, we need a default language
+
+const getTranslate = function (appLanguageName) {
+
+  var appLanguage = localizations[ appLanguageName ];  // Language for the app
+  if (appLanguage === undefined) {
+    throw new ReferenceError('There\'s no localization for ' + appLanguageName);
+  }
 
   /**
    * Returns the text in the default language unless
-   * an overriding language is handed in, in which
-   * case, that's used.
+   * an overriding language is given, in which case
+   * that's used.
    */
-  const translate = function (textKey, languageOverride) {
+  const translate = function (textKeys, languageOverrideName) {
 
-    var language    = languageOverride || appLanguage,
-        translation = localizations[ language ][ textKey ];
+    console.log(1, appLanguage);
+
+    var thisLang    = appLanguage,
+        translation = null,
+        override    = null,
+        langName    = null,
+        message     = null;
+
+    // Special language for just this time
+    if (languageOverrideName) {
+
+      override = localizations[ languageOverrideName ];
+      if (override) {
+        thisLang = override;
+      } else {
+        console.warn('There\'s no localization for ' + languageOverrideName);
+      }
+
+    }
+
+    // The desired translation
+    translation = get(thisLang, textKeys);
+
+    // Fallback to English with warning
+    if (translation === undefined) {
+
+      langName = thisLang.langName;
+      message  = 'There\'s no snippet at ' + textKeys + ' in ' + langName;
+      console.warn(message);
+
+      translation = get(english, textKeys);
+
+    }
+
+    if (translation === undefined) {
+      // Nothing worked, invalid object path
+      // If the language wasn't English, make it clear that it
+      // didn't exist in English either
+      if (langName !== 'English') {
+        message += 'or English';
+      }
+
+      throw new ReferenceError(message);
+    }
 
     return translation;
 
-  };
+  };  // End translate()
 
   return translate;
 
-};
+};  // End getTranslate()
 
 
 export { getTranslate };
