83a268c9cca1a8d56b906675b282a61896724344
Ethan Blackwood <ethanbblackwood@gmail.com>
2018-05-19 20:30:07 -0400

    Restore new English snippets that got deleted somehow

--- src/localization/en.js
+++ src/localization/en.js
@@ -1,11 +1,58 @@
 export default {
-  langName: 'English',
-  langCode: 'en',
-  hasHousing: {
-    label: 'Do you have Section 8 Housing?',
-    hint:  'Section 8 provides rental housing assistance.',
+
+  'langName':    'English',
+  'langCode':    'en',
+  'hasSection8': {
+    'label': 'Do you have Section 8 Housing?',
+    'hint':  'Section 8 provides rental housing assistance.',
   },
-  hasSnap: {
-    label: 'Do you have SNAP?',
+  'hasSnap': {
+    'label': 'Do you have SNAP?',
+    'hint':  'SNAP provides assistance with buying food',
   },
+  'hasEarnedIncome': {
+    'label': 'Do you have earned income?',
+    'hint':  'Earned income is income from wages or self-employment',
+  },
+  'hasTAFDC': {
+    'label': 'Do you have TAFDC?',
+    'hint':  'Transitional Aid to Families with Dependent Children provides short-term financial assistance to families with children',
+  },
+  'hasSSI': {
+    'label': 'Do you have SSI?',
+    'hint':  'Supplemental Security Income is a federal program that provides financial and health care assistance to people 65 and over, or people who are blind or disabled',
+  },
+  'hasSSDI': {
+    'label': 'Do you have SSDI?',
+    'hint':  'Social Security Disability Income is a federal program to help people with disabilities',
+  },
+  'hasChildSupport': {
+    'label': 'Do you receive child support?',
+    'hint':  'Child support is money paid to you by a former spouse to help your child',
+  },
+  'hasUnemployment': {
+    'label': 'Do you receive unemployment benefits?',
+    'hint':  'Unemployment benefits provide income to people who have been laid off',
+  },
+  'hasWorkersComp': {
+    'label': 'Do you receive Workers Compensation?',
+    'hint':  'Workers Compensation provides assistance for people who have been injured on the job',
+  },
+  'hasPension': {
+    'label': 'Do you have a pension?',
+    'hint':  'A pension provides income to retirees, usually from their former employers',
+  },
+  'hasSocialSecurity': {
+    'label': 'Do you have Social Security Income?',
+    'hint':  'Social Security is a federal program that provides assistance to retirees',
+  },
+  'hasAlimony': {
+    'label': 'Do you receive alimony?',
+    'hint':  'Alimony is money paid by one spouse to the other after a divorce',
+  },
+  'hasOtherIncome': {
+    'label': 'Do you have other income?',
+    'hint':  'Please note income you may have from sources that are not listed above',
+  },
+
 };
