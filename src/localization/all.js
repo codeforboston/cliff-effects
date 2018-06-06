6e9c3e8dbc3b931d56d3a3df389f8509f6f6a9a2
Vi Tran <vipran@gmail.com>
2018-06-06 00:07:21 -0400

    gotta call mom for these translations

--- src/localization/all.js
+++ src/localization/all.js
@@ -3,11 +3,13 @@
 import DE from './de';
 import EN from './en';
 import ZH from './zh';
+import VN from './vn';
 
 var localizations = {
   de: DE,
   en: EN,
   zh: ZH,
+  vn: VN,
 };
 
 export { localizations };
