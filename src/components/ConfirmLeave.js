c5855fb0144bfffe455fe1d5d7023787a0f9043b
ethanstrominger <32078396+ethanstrominger@users.noreply.github.com>
2018-06-09 22:30:12 -0400

    Makes changes for lint msgs issue #507 - ConfirmLeave.js

--- src/components/ConfirmLeave.js
+++ src/components/ConfirmLeave.js
@@ -12,7 +12,9 @@ class ConfirmLeave extends React.Component {
   static defaultProps = { isBlocking: true };
 
   confirm = (event) => {
-    if (!this.props.isBlocking) {return;} // do not block unload
+    if (!this.props.isBlocking) {
+      return; // do not block unload
+    } 
     return (event.returnValue = this.props.message || '');
   };
 
