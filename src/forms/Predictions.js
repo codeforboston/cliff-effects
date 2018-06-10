bf2130d850f9812dae8a6dd050ff9b5c5834e70b
knod <knodding@gmail.com>
2018-06-10 15:27:59 -0400

    `return` statement on own lines

--- src/forms/Predictions.js
+++ src/forms/Predictions.js
@@ -171,23 +171,27 @@ const TabbedVisualizations = ({ client }) => {
         { menuItem: 'Changes Chart', render: () => {return <Tab.Pane><Chart client={ client } /></Tab.Pane>;} },
         {
           menuItem: 'Stacked Incomes',
-          render:   () => { return (
-            <Tab.Pane>
-              <GraphHolder
-                client={ client }
-                Graph={ GrossGraph } />
-            </Tab.Pane>
-          );},
+          render:   () => {
+            return (
+              <Tab.Pane>
+                <GraphHolder
+                  client={ client }
+                  Graph={ GrossGraph } />
+              </Tab.Pane>
+            );
+          },
         },
         {
           menuItem: 'Benefit Programs',
-          render:   () => { return (
-            <Tab.Pane>
-              <GraphHolder
-                client={ client }
-                Graph={ BenefitGraph } />
-            </Tab.Pane>
-          );}, 
+          render:   () => {
+            return (
+              <Tab.Pane>
+                <GraphHolder
+                  client={ client }
+                  Graph={ BenefitGraph } />
+              </Tab.Pane>
+            );
+          }, 
         },
       ] } />
   );
