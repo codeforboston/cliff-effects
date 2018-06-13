6774a4071d74d91008563a5260c0162d55d6b019
Vi Tran <vipran@gmail.com>
2018-06-12 21:33:53 -0400

    added a few viet translation

---
+++ src/localization/vn.js
@@ -0,0 +1,98 @@
+export default {
+
+    langName: 'Tiếng Việt',
+    langCode: 'vn',
+  
+    header: {},
+  
+    footer: {
+      header:         'Cliff Effects Tool',
+      cfbCreditIntro: 'Made with ',
+      cfbCredit:      '  Code for Boston',
+    },
+  
+    homePage: {
+      appName:          'Cliff Effects Tool',
+      prototypeNote:    'GUIDANCE PROTOYPE*',
+      cautionaryNote:   '*This is a prototype and should not be used to make financial decisions',
+      toFirstInputPage: 'Bắt đầu',
+      toAboutPage:      'Tìm hiểu thêm',
+    },
+  
+    aboutPage: {},
+  
+    visitPage: {
+      stepBar: {
+        currentBenefits: 'Trợ cấp hiện tại',
+        household:       'Gia đình',
+        currentIncome:   'Tiền lương',
+        currentExpenses: 'Chi phí',
+        predictions:     'Dự đoán',
+      },
+  
+      currentBenefits: {
+        hasSection8: {
+          label: 'Bạn dược hưởng chương trình trợ cấp nha` ở thuộc section 8 phải không?',
+          hint:  'Chương trình Section 8 trợ giúp tiền thuê nha`.'
+        },
+        hasSnap: {
+          label: 'Bạn có được nhận SNAP không?',
+          hint:  'SNAP la` chương trình giúp đỡ tiền mua thực phẩm.',
+        },
+      },
+  
+      household: {},
+  
+      currentIncome: {
+        earnedIncome: {
+          label: 'Earned income',
+          hint:  'Earned income la` tiền trợ giúp cho người lương thấp, có con còn nhỏ, đdang đi học hoặc bị tàn tật',
+        },
+        TAFDC: {
+          label: 'TAFDC',
+          hint:  'Transitional Aid to Families with Dependent Children, TAFDC la` tiền trợ giúp ngắn hạn cho gia dình có con nhỏ.',
+        },
+        SSI: {
+          label: 'SSI',
+          hint:  'Supplemental Security Income is a federal program that provides financial and health care assistance to people 65 and over, or people who are blind or disabled',
+        },
+        SSDI: {
+          label: 'SSDI',
+          hint:  'Social Security Disability Income is a federal program to help people with disabilities',
+        },
+        childSupport: {
+          label: 'Child support recieved',
+          hint:  'Child support la` tiền nhận được do người phối ngẫu',
+        },
+        unemployment: {
+          label: 'Unemployment',
+          hint:  'Unemployment benefits provide income to people who have been laid off',
+        },
+        workersComp: {
+          label: 'Worker\'s compensation',
+          hint:  'Worker\'s Compensation provides assistance for people who have been injured on the job',
+        },
+        pension: {
+          label: 'Pension',
+          hint:  'A pension provides income to retirees, usually from their former employers',
+        },
+        socialSecurity: {
+          label: 'Social security',
+          hint:  'Social Security is a federal program that provides assistance to retirees',
+        },
+        alimony: {
+          label: 'Alimony',
+          hint:  'Alimony is money paid by one spouse to the other after a divorce',
+        },
+        otherIncome: {
+          label: 'Other income',
+          hint:  'Please note income you may have from sources that are not listed above',
+        },
+      },
+  
+      currentExpenses: {},
+  
+      predictions: {},
+    },
+  };
+  
