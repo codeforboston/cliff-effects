40aeea4ddf82822b3adc8675dc7e9237363b9e8d
knod <knodding@gmail.com>
2018-07-12 08:47:09 -0400

    Adds new 'Benefits' snapshot

---
+++ src/test/forms/CurrentBenefits.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { CurrentBenefitsStep } from '../../forms/CurrentBenefits';
+
+import { CLIENT_DEFAULTS } from '../../utils/CLIENT_DEFAULTS';
+import snippets from '../../localization/en.js';
+
+test('Benefits step component renders as snapshot correctly', () => {
+  const navData = {
+          left:   null,
+          middle: null,
+          right:  { text: 'Next', onClick: jest.fn() },
+        },
+        changeClient     = jest.fn(),
+        saveForm         = jest.fn(),
+        askToResetClient = jest.fn(),
+        openFeedback     = jest.fn(),
+        formSnippets     = snippets.visitPage.currentBenefits;
+
+  const wrapper = shallow(
+    <CurrentBenefitsStep
+      currentStep       = { 1 }
+      client            = { CLIENT_DEFAULTS }
+      navData           = { navData }
+      changeClient      = { changeClient }
+      saveForm          = { saveForm }
+      askToResetClient  = { askToResetClient }
+      openFeedback      = { openFeedback }
+      snippets          = { formSnippets } />
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
