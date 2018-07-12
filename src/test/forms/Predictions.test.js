b73854014ba749bea65544b1284398d1605dcf22
knod <knodding@gmail.com>
2018-07-12 08:46:32 -0400

    Updates 'Predictions' snapshot test to new format

--- src/test/forms/Predictions.test.js
+++ src/test/forms/Predictions.test.js
@@ -4,22 +4,31 @@ import { shallow } from 'enzyme';
 import { PredictionsStep } from '../../forms/Predictions';
 
 import { CLIENT_DEFAULTS } from '../../utils/CLIENT_DEFAULTS';
+import snippets from '../../localization/en.js';
 
 test('Prediction component renders as snapshot correctly', () => {
-  const nextStep = jest.fn();
-  const previousStep = jest.fn();
-  const setClientProperty = jest.fn();
-  const changeClient = jest.fn();
-  const saveForm = jest.fn();
+  const navData = {
+          left:   { text: 'Previous', onClick: jest.fn() },
+          middle: null,
+          right:  { text: 'New Client', onClick: jest.fn() },
+        },
+        changeClient     = jest.fn(),
+        saveForm         = jest.fn(),
+        askToResetClient = jest.fn(),
+        openFeedback     = jest.fn(),
+        formSnippets     = snippets.visitPage.predictions;
+
   const wrapper = shallow(
     <PredictionsStep
-      currentStep={ 1 }
-      client={ CLIENT_DEFAULTS }
-      nextStep={ nextStep }
-      previousStep={ previousStep }
-      setClientProperty={ setClientProperty }
-      changeClient={ changeClient }
-      saveForm={ saveForm } />
+      currentStep       = { 5 }
+      client            = { CLIENT_DEFAULTS }
+      navData           = { navData }
+      changeClient      = { changeClient }
+      saveForm          = { saveForm }
+      askToResetClient  = { askToResetClient }
+      openFeedback      = { openFeedback }
+      snippets          = { formSnippets } />
   );
+
   expect(wrapper).toMatchSnapshot();
 });
