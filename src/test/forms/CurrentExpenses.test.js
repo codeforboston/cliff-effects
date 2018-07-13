b70a0dadb4b8ba8829fbaa2a16671121d88254df
knod <knodding@gmail.com>
2018-07-13 14:54:38 -0400

    Adds snapshot test for CurrentBenefits

---
+++ src/test/forms/CurrentExpenses.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { CurrentExpensesStep } from '../../forms/CurrentExpenses';
+
+import { CLIENT_DEFAULTS } from '../../utils/CLIENT_DEFAULTS';
+import snippets from '../../localization/en.js';
+
+test('Expenses step component renders as snapshot correctly', () => {
+  const navData = {
+          left:   { text: 'Previous', onClick: jest.fn() },
+          middle: null,
+          right:  { text: 'Next', onClick: jest.fn() },
+        },
+        changeClient     = jest.fn(),
+        saveForm         = jest.fn(),
+        askToResetClient = jest.fn(),
+        openFeedback     = jest.fn(),
+        formSnippets     = snippets.visitPage.currentExpenses;
+
+  const wrapper = shallow(
+    <CurrentExpensesStep
+      currentStep       = { 2 }
+      client            = { CLIENT_DEFAULTS }
+      navData           = { navData }
+      changeClient      = { changeClient }
+      saveForm          = { saveForm }
+      askToResetClient  = { askToResetClient }
+      openFeedback      = { openFeedback }
+      snippets          = { formSnippets } />
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
