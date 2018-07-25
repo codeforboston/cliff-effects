cccf4435509ea5c1721566a702ea2ad3c5305356
Ethan Blackwood <ethanbblackwood@gmail.com>
2018-07-24 21:21:58 -0400

    Move inline components for interpolation and contributor names to separate files under localization

--- src/localization/all.js
+++ src/localization/all.js
@@ -1,5 +1,8 @@
 /** Contains and exports all the languages */
 
+import { interpolateSnippets } from '../utils/interpolation';
+import inlineComponents from './inlineComponents';
+
 import DE from './de';
 import EN from './en';
 import ZH from './zh';
@@ -9,5 +12,11 @@ var localizations = {
   en: EN,
   zh: ZH,
 };
+
+// Interpolate inline Components
+for (const lang in localizations) {
+  var snippets = localizations[ lang ];
+  localizations[ lang ] = interpolateSnippets(snippets, inlineComponents);
+}
 
 export { localizations };
