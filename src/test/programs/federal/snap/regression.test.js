6d07cbdfc8979f582171db2fe51a33bd2131cbe8
knod <knodding@gmail.com>
2018-08-03 15:42:53 -0400

    Removes work to pin down crazy test issues

--- src/test/programs/federal/snap/regression.test.js
+++ src/test/programs/federal/snap/regression.test.js
@@ -9,23 +9,15 @@ import { getSNAPBenefits } from '../../../../programs/federal/snap';
 
 test('getSNAPBenefits() matches saved results', (done) => {
   const rl = readline.createInterface({ input: fs.createReadStream(path.resolve(__dirname, 'test-cases.txt')) });
-  // console.log('blah');
+
   rl.on('line', (line) => {
-    // console.log(line);
-    // try {
-      const [
-        setupStr,
-        expectedStr, 
-      ] = line.split(';');
-      const client = cloneDeep(CLIENT_DEFAULTS);
-      extend(client.current, JSON.parse(setupStr));
-      expect(getSNAPBenefits(client, 'current')).toEqual(+expectedStr);
-    // } catch (err) {
-    //   console.log('======================\n======================\n======================\n======================\n======================\n======================\n======================\n======================\n======================\n');
-    //   console.log(line);
-    //   console.log('======================\n======================\n======================\n======================\n======================\n======================\n======================\n======================\n======================\n');
-    // }
-    
+    const [
+      setupStr,
+      expectedStr, 
+    ] = line.split(';');
+    const client = cloneDeep(CLIENT_DEFAULTS);
+    extend(client.current, JSON.parse(setupStr));
+    expect(getSNAPBenefits(client, 'current')).toEqual(+expectedStr);
   });
 
   rl.on('close', done);
