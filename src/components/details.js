1114a0ef2f599a48f4daa436883bf59615312ffb
knod <knodding@gmail.com>
2018-08-05 11:27:30 -0400

    Safer state, child check, and description

--- src/components/details.js
+++ src/components/details.js
@@ -6,26 +6,42 @@ import {
 } from 'semantic-ui-react';
 
 
+/** Really it's a top component that has a question mark
+ *    next to it followed by a bottom one that starts
+ *    as hidden, but is revealed when the question mark
+ *    is clicked.
+ *
+ * @param {object} props
+ * @param {object} props.children Must have exactly two
+ *    children. The first is always visible. The second
+ *    is revealed when the icon is clicked and then hidden
+ *    when the icon is clicked again.
+ */
 class HeadingWithDetail extends Component {
   state = { showDetails: false };
 
   toggleDetails = () => {
-    if (this.state.showDetails) {
-      this.setState({ showDetails: false });
-    } else {
-      this.setState({ showDetails: true });
-    }
+    this.setState((prevState) => {
+      return { showDetails: !prevState.showDetails };
+    });
   };
 
   render () {
     var { showDetails } = this.state;
 
-    var children  = this.props.children,
-        top       = children[ 0 ] || children,
+    var children  = this.props.children;
+    if (!Array.isArray(children) || children.length !== 2) {
+      console.warn(`<HeadingWithDetail> must have exactly two children`);
+      return null;
+    }
+
+    var top       = children[ 0 ] || children,
         details   = children[ 1 ] || null,
+        // Has a green question mark icon by default
         iconName  = `question circle outline`,
         iconColor = `teal`;
 
+    // Show a red 'x' instead if the detail is visible right now
     if (showDetails) {
       iconName  = `remove circle`;
       iconColor = `red`;
