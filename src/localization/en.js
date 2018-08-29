b2a5cff6347dfbe43fbb4d1e7afc670e32741190
royliu245 <royliu245@gmail.com>
2018-08-28 20:27:05 -0400

    Fixes #723: Clarify child support expenses question

--- src/localization/en.js
+++ src/localization/en.js
@@ -219,7 +219,7 @@ export default {
         columnExpenseType:   `Expense`,
         childSupportPaidOut: {
           legallyObligated: `Legally obligated`,
-          childSupport:     ` child support`,
+          childSupport:     ` child support paid out`,
         },
       },
       housing: {
