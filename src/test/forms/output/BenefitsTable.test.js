9aa158572f65d16ffb210152bd0642dc9029ee34
Andrew Cunningham <w.a.cunningham.ii@gmail.com>
2018-10-09 21:25:56 -0400

    Extend BenefitsTable tests

--- src/test/forms/output/BenefitsTable.test.js
+++ src/test/forms/output/BenefitsTable.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { cloneDeep, set } from 'lodash';
 
 import { BenefitsTable } from '../../../forms/output/BenefitsTable';
 
@@ -8,13 +9,37 @@ import { CLIENT_DEFAULTS } from '../../../utils/CLIENT_DEFAULTS';
 // LOCALIZATION
 import { getTextForLanguage } from '../../../utils/getTextForLanguage';
 
-
 var snippets = getTextForLanguage(`en`);
-test('Benefits table renders correctly', () => {
+
+const buildSnapshot = (client) => {
   const rendered = renderer.create(
     <BenefitsTable 
-      client={ CLIENT_DEFAULTS } 
+      client={ client } 
       snippets={ snippets.visitPage.predictions } />
   );
-  expect(rendered.toJSON()).toMatchSnapshot();
+
+  return rendered.toJSON();
+};
+
+test('Benefits table renders correctly', () => {
+  const client = cloneDeep(CLIENT_DEFAULTS);
+  expect(buildSnapshot(client)).toMatchSnapshot();
+
+  set(client, 'current.hasSnap', true);
+  expect(buildSnapshot(client)).toMatchSnapshot();
+
+  set(client, 'current.earned', 100);
+  expect(buildSnapshot(client)).toMatchSnapshot();
+
+  set(client, 'future.earned', 200);
+  expect(buildSnapshot(client)).toMatchSnapshot();
+
+  set(client, 'current.earned', 300);
+  expect(buildSnapshot(client)).toMatchSnapshot();
+
+  set(client, 'current.hasSection8');
+  expect(buildSnapshot(client)).toMatchSnapshot();
+
+  set(client, 'future.earned', 400);
+  expect(buildSnapshot(client)).toMatchSnapshot();
 });
