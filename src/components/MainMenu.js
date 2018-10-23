9778d9b6dd27e8cac70777cde9093ecef547dc13
Michael Caveney <dylanesque27@gmail.com>
2018-10-23 19:59:49 -0400

    Edited MainMenu.js to fix merge conflicts

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,30 +1,27 @@
 import React from 'react';
 import {
   // Button,
-  Container,
   Menu,
 } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-import { BetaWarning } from './BetaWarning';
 
 
-const MainMenu = function (props) {
+const MainMenu = function ({ snippets }) {
   return (
-    <Container>
-      <Menu
-        inverted
-        secondary
-        size='large'>
 
-        <Menu.Item><Link className="main-nav" to="/">Home</Link></Menu.Item>
-        <Menu.Item><Link className="main-nav" to="/about">About</Link></Menu.Item>
-        <Menu.Item position='right'>
-          {/*<Link to="/login"><Button inverted>Log in</Button></Link>*/}
-          {/*<Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>*/}
-        </Menu.Item>
-        <BetaWarning />
-      </Menu>
-    </Container>
+    <Menu
+      inverted
+      secondary
+      size='large'>
+
+      <Menu.Item><Link className="main-nav" to="/">{ snippets.i_homeNav }</Link></Menu.Item>
+      <Menu.Item><Link className="main-nav" to="/about">{ snippets.i_aboutNav }</Link></Menu.Item>
+      <Menu.Item>{ snippets.i_githubNav }</Menu.Item>
+      <Menu.Item position='right'>
+        {/*<Link to="/login"><Button inverted>Log in</Button></Link>*/}
+        {/*<Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>*/}
+      </Menu.Item>
+    </Menu>
   );
 };  // End MainMenu(<>)
 
