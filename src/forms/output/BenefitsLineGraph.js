37a8208128815fc8a9b20865d02388222aa4f7f0
Turner Hayes <dev@turnerhayes.com>
2018-11-10 19:43:47 -0500

    Fix BenefitsLineGraph improperly filling areas

--- src/forms/output/BenefitsLineGraph.js
+++ src/forms/output/BenefitsLineGraph.js
@@ -47,13 +47,14 @@ class BenefitsLineGraph extends Component {
     var max       = Math.ceil((limits.max * multiplier) / 100) * 100,
         interval  = Math.ceil((max / 100) / 10) * 10;
 
-    var xRange      = _.range(limits.min, max, interval),  // x-axis/earned income numbers
-        extraProps  = {},
-        datasets    = getChartData(xRange, multiplier, client, activePrograms, extraProps);
+    let extraProps  = {};
     
     for (let benefitIndex = 0; benefitIndex < activePrograms.length; benefitIndex++) {
       extraProps[ activePrograms[ benefitIndex ] ] = { fill: false };
     }
+
+    var xRange      = _.range(limits.min, max, interval),  // x-axis/earned income numbers
+        datasets    = getChartData(xRange, multiplier, client, activePrograms, extraProps);
 
     // If there's no data to show, don't show the table
     if (datasets.length === 0) {
