ecf85efab1b2f5067b6d163bdeae88f682f55f4c
knod <knodding@gmail.com>
2018-11-24 23:32:43 -0500

    Cleans code style of '/src/components' through 'headers.js`. #1018

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from '../images/logo.svg';
-
 import {
   Grid,
   Header,
@@ -8,42 +6,49 @@ import {
   Image,
 } from 'semantic-ui-react';
 
-const Footer = ({ translations }) => {
+import logo from '../images/logo.svg';
+
+
+const Footer = function ({ translations }) {
   return (
     <Segment
       className = { `footer_segment` }
+      color     = { `teal` }
       inverted
-      vertical
-      color='teal'>
+      vertical>
       <Grid
         container
         divided
         inverted
         stackable>
         <Grid.Row>
+
           <Grid.Column
-            width={ 4 }
-            floated='left'>
+            width   = { 4 }
+            floated = { `left` }>
             <Header
-              as='h4'
+              as = { `h4` }
               inverted>
               { translations.i_header }
             </Header>
             <p>{ translations.i_cfbCredit }</p>
           </Grid.Column>
-          <a
 
-            href="http://www.codeforboston.org"
-            target="_blank"
-            rel="noopener noreferrer"><Image
-              src={ logo }
-              size='small'
-              floated='right' />
+          <a
+            href   = { `http://www.codeforboston.org` }
+            target = { `_blank` }
+            rel    = { `noopener noreferrer` }>
+            <Image
+              src     = { logo }
+              size    = { `small` }
+              floated = { `right` } />
           </a>
+
         </Grid.Row>
       </Grid>
     </Segment>
   );
-};
+};  // Ends <Footer>
 
-export default Footer;
+
+export { Footer };
