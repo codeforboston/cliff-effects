e1619e6dcb159f910e84df4496fc7e460ffee6b5
knod <knodding@gmail.com>
2018-11-27 20:51:52 -0500

    Fix netlify because of docs?

--- src/data/federal/federalPovertyGuidelines.js
+++ src/data/federal/federalPovertyGuidelines.js
@@ -2,10 +2,10 @@
  * @namespace
  *
  * RESOURCES:
- * {@link http://www.mass.gov/eohhs/docs/masshealth/deskguides/fpl-deskguide.pdf}
  * {@link https://aspe.hhs.gov/poverty-guidelines}
  */
 const federalPovertyGuidelines = { 0: 0, 1: 12060, 2: 16240, 3: 20420, 4: 24600, 5: 28780, 6: 32960, 7: 37140, 8: 41320, 'eachAdditional': 4180 };
+// {@link http://www.mass.gov/eohhs/docs/masshealth/deskguides/fpl-deskguide.pdf}
 
 
 export { federalPovertyGuidelines };
