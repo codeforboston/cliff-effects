a62b2e1773a5f17cb9e28a918cbcf32772749252
knod <3197142-knod@users.noreply.gitlab.com>
2019-01-02 08:19:38 -0500

    Re-uses/places abstraction for formatting chart values in Summary

--- src/forms/predictions/Summary.js
+++ src/forms/predictions/Summary.js
@@ -7,16 +7,11 @@ import {
   Button,
 } from 'semantic-ui-react';
 
-// DATA
-// Colors and text for parts of the chart
-import { BENEFIT_CHART_VALUES } from './BENEFIT_CHART_VALUES';
-
-// DATA MANIPULATION
-import { cloneDeep } from 'lodash';
-import { toMoneyStr } from '../../utils/prettifiers';
-
-// BENEFIT LOGIC
+// NON-COMPONENTS
 import { applyAndPushBenefits } from '../../benefits/applyAndPushBenefits';
+import { toMoneyStr } from '../../utils/prettifiers';
+import { calcDataToChartData } from './build-resource-data';
+import { cloneDeep } from 'lodash';
 
 
 const EARNED_MONTHLY_INCREMENT_AMOUNT = 50;  // About a 25 cent raise in monthly amount for 40hrs/week?
@@ -49,105 +44,6 @@ let totalLastItemsOfArraysInObject = function (accumulated) {
   }
   return total;
 };
-
-/** Returns values representing earned income and benefit
- *     amounts at the given index, as well as the sum of all
- *     those benefits and the sum of the earned income and
- *     those benefits.
- *
- * @param {array} keys Contains keys to use on `sourceObject`.
- * @param {object} sourceObject MUST CONTAIN `earned` property!
- *     Contains `earned` and benefit keys that each have an
- *     array of numerical values (which are meant to be money
- *     values right now).
- * @param {array} sourceObject.earned Earned income values.
- * @param {int} Which item in each array should be used to
- *      accumulate values.
- *
- * @example
- * // In BENEFIT_CHART_VALUES.js
- * let BENEFIT_CHART_VALUES = {
- *  benefit1: { name: "B1" },
- *  benefit2: { name: "B2" },
- * };
- * 
- * // In here
- * let keys = [
- *  'benefit1',
- *  'benefit2',
- * ];
- *
- * let accumulated = {
- *  earned:   [ 450, 500 ],
- *  benefit1: [ 80, 30 ],
- *  benefit2: [ 40, 10 ],
- * };
- * 
- * let index = 1;
- * 
- * var summaryData = fillInMoneyValues(keys, accumulated, index);
- * 
- * console.log(summaryData);
- * // {
- * //   earned: 500,
- * //   benefits: [
- * //     { label: "B1", amount: 30 },
- * //     { label: "B2", amount: 10 }
- * //   ],
- * //   benefitsTotal: 40,
- * //   total: 540,
- * // }
- * 
- * Unfortunately, stll relies on an outside value -
- *     BENEFIT_CHART_VALUES.
- * 
- * @typedef {object} benefit
- * @property {string} label Name to be displayed for the benefit
- * @property {number} amount Value of the benefit/subsidy
- *
- * @typedef {object} moneyValues
- * @property {number} earned Amount earned at a given index
- * @property {array.<benefit>} benefits
- * @property {number} benefitsTotal Sum of all benefit values at
- *     the given index
- * @property {number} total Sum of earned income and all benefits
- *     at the given index
- * 
- * @returns {object} moneyValues
- */
-let fillInMoneyValues = (keys, sourceObject, index) => {
-
-  if (!Array.isArray(sourceObject.earned)) {
-    throw new TypeError(`The given resources object requires an 'earned' property that is an array of numbers.`);
-  }
-
-  let moneyValues = {
-    earned:        0,
-    benefits:      [],  // [{ label, amount }]
-    benefitsTotal: 0,
-    total:         0,
-  };
-
-  // Item names can be `earned` or benefit keys
-  for (let itemKey of keys) {
-    let amount = sourceObject[ itemKey ][ index ];
-
-    if (itemKey === `earned`) {
-      moneyValues.earned = amount;
-    } else {
-      moneyValues.benefits.push({
-        label:  BENEFIT_CHART_VALUES[ itemKey ].name,
-        amount: amount,
-      });
-      // Add up all benefits (we're not including earned income)
-      moneyValues.benefitsTotal += amount;
-    }
-  }  // ends for every item key name
-
-  moneyValues.total = moneyValues.earned + moneyValues.benefitsTotal;
-
-  return moneyValues;
-};  // Ends fillInMoneyValues()
 
 
 /** Uses benefit data getter and reformats it to
@@ -224,8 +120,8 @@ let getBenefitData = function(client, resourceKeys) {
 
   // 2. Get totals
   // Fill earned values for both current and future earned objects
-  result.current = fillInMoneyValues(resourceKeys, accumulated, 0);
-  result.future  = fillInMoneyValues(resourceKeys, accumulated, 1);
+  result.current = calcDataToChartData(resourceKeys, accumulated, 0);
+  result.future  = calcDataToChartData(resourceKeys, accumulated, 1);
   let resultCurr = result.current,
       resultFutr = result.future;
 
@@ -434,6 +330,5 @@ const Summary = function ({ client, openFeedback, translations }) {
 export {
   Summary,
   totalLastItemsOfArraysInObject,
-  fillInMoneyValues,
   getBenefitData,  
 };
